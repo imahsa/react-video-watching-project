5db0f78956e0e041c184e5da4c4c17e0ec757b76
Mahsa Mohammadi <mohammadi1@outlook.com>
2017-11-08 15:55:26 +1100

    Search feature is added. CSS styling improved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+ import _ from 'lodash';
  import React, {Component}  from 'react';
  import ReactDOM from 'react-dom';
  import SearchBar from './components/search_bar';
@@ -11,18 +12,33 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],  
+			selectedVideo: null
+		};
 		
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-				this.setState({videos});
+		this.videoSearch('surfboards');
+	}		
+	
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
-	}		// ES16 syntax this.setState({videos: videos}), when key and property are in the same name
+	}
+
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500 );
 		return(
  		<div>
- 			<SearchBar />
- 			<VideoDetails video={this.state.videos[0]} />
- 			<VideoList videos={this.state.videos} />
+ 			<SearchBar onSearchTermChanged={videoSearch}  />
+ 			<VideoDetails video={this.state.selectedVideo} />
+ 			<VideoList
+ 				onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
+ 				videos={this.state.videos} 
+ 			/>
  		</div>
  		);
 	}		 
